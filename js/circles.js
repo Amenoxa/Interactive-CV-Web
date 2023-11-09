0897a9dc7dbb2607b4ffdbeb9a5f4632f5a21286
Amenoxa <48977442+Amenoxa@users.noreply.github.com>
2023-11-09 12:28:48 +0100

    initial commit

---
+++ js/circles.js
@@ -0,0 +1,25 @@
+anime({
+    targets: '.center_circle--inner',
+    rotate: 360,
+    loop: true,
+    duration: 16000,
+    easing: 'linear'
+  });
+
+  anime({
+    targets: '.center_circle--middle',
+    rotate: -360,
+    loop: true,
+    duration: 8000,
+    easing: 'linear'
+  });
+
+  anime({
+    targets: '.center_circle--outer',
+    rotate: 360,
+    loop: true,
+    duration: 20000,
+    easing: 'linear'
+  });
+
+  
